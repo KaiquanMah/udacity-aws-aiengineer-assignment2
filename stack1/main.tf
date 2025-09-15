c083491b8c338b9120f006faf4f6a73ed24629f4
KaiquanMah <7814298+KaiquanMah@users.noreply.github.com>
2025-09-15 07:12:00 +0000

    updated stack1 to use already created VPC - instead of following default/given information from cloned repo. terraform apply successful

--- stack1/main.tf
+++ stack1/main.tf
@@ -2,42 +2,68 @@ provider "aws" {
   region = "us-east-1"  # Change this to your desired region # UPDATED
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
 
-  name = "bedrock-poc-vpc"
-  cidr = "10.0.0.0/16"
+# ADDED TO FIX PERMISSIONS ISSUE
+# This data block LOOKS UP the existing Default VPC. It does NOT create a new one.
+# This is necessary to get the VPC ID and other details.
+data "aws_vpc" "default" {
+  default = true
+}
+# "vpc-0109f710cbe850abd" 
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"] # UPDATED
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
+# This data block LOOKS UP the existing subnets within the Default VPC found above.
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
+
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "~> 5.0"
+# 
+#   name = "bedrock-poc-vpc"
+#   cidr = "10.0.0.0/16"
+# 
+#   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"] # UPDATED
+#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+# 
+#   enable_nat_gateway = true
+#   single_nat_gateway = true
+# 
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+# 
+#   tags = {
+#     Terraform   = "true"
+#     Environment = "dev"
+#   }
+# }
 
 module "aurora_serverless" {
   source = "../modules/database"
 
   cluster_identifier = "my-aurora-serverless"
-  vpc_id             = module.vpc.vpc_id 
-  subnet_ids         = module.vpc.private_subnets
+  # vpc_id             = module.vpc.vpc_id 
+  # subnet_ids         = module.vpc.private_subnets
+  # UPDATED: Using the ID from the Default VPC we looked up.
+  vpc_id = data.aws_vpc.default.id
+  # UPDATED: Using the IDs from the subnets we looked up in the Default VPC.
+  subnet_ids = data.aws_subnets.default.ids
+
+
 
   # Optionally override other defaults
   database_name    = "myapp"
   master_username  = "dbadmin"
   max_capacity     = 1
   min_capacity     = 0.5
-  allowed_cidr_blocks = ["10.0.0.0/16"]   
+  # allowed_cidr_blocks = ["10.0.0.0/16"]   
+  # UPDATED: This now correctly allows traffic from within the Default VPC.
+  allowed_cidr_blocks = [data.aws_vpc.default.cidr_block]
 }
 
 data "aws_caller_identity" "current" {}
--- stack1/outputs.tf
+++ stack1/outputs.tf
@@ -7,16 +7,28 @@ output "db_reader_endpoint" {
 }
 
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  # value = module.vpc.vpc_security_group_ids
+  # FIXED: Changed from module.vpc.vpc_id to the data source.
+  value       = data.aws_vpc.default.id
 }
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnets
+
+output "default_subnet_ids" {
+  description = "The IDs of all subnets in the default VPC."
+  # FIXED: Changed from module.vpc.private_subnets to the data source.
+  # Note: We are now getting ALL default subnets, not just private ones.
+  value       = data.aws_subnets.default.ids
 }
 
-output "public_subnet_ids" {
-  value = module.vpc.public_subnets
-}
+# REMOVED: The public_subnet_ids output is no longer relevant as we are using
+# the data source which fetches all subnets without distinguishing public/private.
+# output "private_subnet_ids" {
+#   value = module.vpc.private_subnets
+# }
+# 
+# output "public_subnet_ids" {
+#   value = module.vpc.public_subnets
+# }
 
 output "aurora_endpoint" {
   value = module.aurora_serverless.cluster_endpoint
@@ -31,5 +43,8 @@ output "rds_secret_arn" {
 }
 
 output "s3_bucket_name" {
-  value = module.s3_bucket.s3_bucket_arn
+  # value = module.s3_bucket.s3_bucket_arn
+  # FIXED: The module output for the bucket name is "s3_bucket_id".
+  # The original value was pointing to the ARN, which is incorrect for the output name.
+  value       = module.s3_bucket.s3_bucket_id
 }
